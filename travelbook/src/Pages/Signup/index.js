2f02bde51694fbd2c508b82d8fb8c6ba29e5b3b5
Bhuvana K S <bhuvana.ks@rockwellautomation.com>
2024-07-07 19:33:44 +0530

    signup-form

--- travelbook/src/Pages/Signup/index.js
+++ travelbook/src/Pages/Signup/index.js
@@ -1,15 +1,160 @@
-import React from 'react'
-import { Input, Stack } from '@chakra-ui/react'
+// import React from 'react';
+// import { useForm } from 'react-hook-form';
+// import { ChakraProvider, Box, Button, FormControl, FormLabel, Input, FormErrorMessage } from '@chakra-ui/react';
+
+// const Signup = () => {
+//   const { handleSubmit, register, formState: { errors, isSubmitting } } = useForm();
+
+//   const onSubmit = (values) => {
+//     console.log(values);
+//   };
+
+//   return (
+//     <ChakraProvider>
+//       <Box maxW="md" mx="auto" mt={10} p={8} borderWidth={1} borderRadius={8} boxShadow="lg">
+//         <form onSubmit={handleSubmit(onSubmit)}>
+//           <FormControl isInvalid={errors.name}>
+//             <FormLabel htmlFor="name">Name</FormLabel>
+//             <Input
+//               id="name"
+//               placeholder="Name"
+//               {...register("name", {
+//                 required: "Name is required",
+//               })}
+//             />
+//             <FormErrorMessage>
+//               {errors.name && errors.name.message}
+//             </FormErrorMessage>
+//           </FormControl>
+
+//           <FormControl mt={4} isInvalid={errors.email}>
+//             <FormLabel htmlFor="email">Email</FormLabel>
+//             <Input
+//               id="email"
+//               type="email"
+//               placeholder="Email"
+//               {...register("email", {
+//                 required: "Email is required",
+//                 pattern: {
+//                   value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
+//                   message: "Invalid email address",
+//                 },
+//               })}
+//             />
+//             <FormErrorMessage>
+//               {errors.email && errors.email.message}
+//             </FormErrorMessage>
+//           </FormControl>
+
+//           <FormControl mt={4} isInvalid={errors.password}>
+//             <FormLabel htmlFor="password">Password</FormLabel>
+//             <Input
+//               id="password"
+//               type="password"
+//               placeholder="Password"
+//               {...register("password", {
+//                 required: "Password is required",
+//                 minLength: { value: 6, message: "Password must be at least 6 characters" },
+//               })}
+//             />
+//             <FormErrorMessage>
+//               {errors.password && errors.password.message}
+//             </FormErrorMessage>
+//           </FormControl>
+
+//           <Button mt={4} colorScheme="teal" isLoading={isSubmitting} type="submit">
+//             Sign Up
+//           </Button>
+//         </form>
+//       </Box>
+//     </ChakraProvider>
+//   );
+// };
+
+// export default Signup;
+import React from 'react';
+import { useForm } from 'react-hook-form';
+import { ChakraProvider, Box, Button, FormControl, FormLabel, Input, FormErrorMessage } from '@chakra-ui/react';
+
 const Signup = () => {
+  const { handleSubmit, register, formState: { errors, isSubmitting } } = useForm();
+
+  const onSubmit = (values) => {
+    console.log(values);
+  };
+
   return (
-    <div>
-    <Stack spacing={3}>
+    <ChakraProvider>
+      <Box maxW="md" mx="auto" mt={10} p={8} borderWidth={1} borderRadius={8} boxShadow="lg">
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <FormControl>
+            <FormLabel htmlFor="name">Name</FormLabel>
+            <Input
+              id="name"
+              placeholder="Name"
+              {...register("name")}
+            />
+          </FormControl>
 
-        <Input variant='filled' placeholder='Name' size='md' htmlSize={4} width="20rem"/>
+          <FormControl mt={4} isInvalid={errors.email}>
+            <FormLabel htmlFor="email">Email</FormLabel>
+            <Input
+              id="email"
+              type="email"
+              placeholder="Email"
+              {...register("email", {
+                required: "Email is required",
+                pattern: {
+                  value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
+                  message: "Invalid email address",
+                },
+              })}
+            />
+            <FormErrorMessage>
+              {errors.email && errors.email.message}
+            </FormErrorMessage>
+          </FormControl>
 
-</Stack>
-    </div>
-  )
-}
+          <FormControl mt={4}>
+            <FormLabel htmlFor="password">Password</FormLabel>
+            <Input
+              id="password"
+              type="password"
+              placeholder="Password"
+              {...register("password", {
+                minLength: { value: 6, message: "Password must be at least 6 characters" },
+              })}
+            />
+          </FormControl>
 
-export default Signup
+          <FormControl mt={4}>
+            <FormLabel htmlFor="age">Age</FormLabel>
+            <Input
+              id="age"
+              type="number"
+              placeholder="Age"
+              {...register("age", {
+                min: { value: 0, message: "Age must be a positive number" },
+              })}
+            />
+          </FormControl>
+
+          <FormControl mt={4}>
+            <FormLabel htmlFor="dob">Date of Birth</FormLabel>
+            <Input
+              id="dob"
+              type="date"
+              {...register("dob")}
+            />
+          </FormControl>
+
+          <Button mt={4} colorScheme="teal" isLoading={isSubmitting} type="submit">
+            Sign Up
+          </Button>
+        </form>
+      </Box>
+    </ChakraProvider>
+  );
+};
+
+export default Signup;
