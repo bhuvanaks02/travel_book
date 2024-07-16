ea07048e70df35b3eb59e9edc0b35d5f68039df7
Bhuvana K S <bhuvana.ks@rockwellautomation.com>
2024-07-16 23:53:39 +0530

    firebase-added

--- travelbook/src/Pages/Signup/index.js
+++ travelbook/src/Pages/Signup/index.js
@@ -1,84 +1,19 @@
-// import React from 'react';
-// import { useForm } from 'react-hook-form';
-// import { ChakraProvider, Box, Button, FormControl, FormLabel, Input, FormErrorMessage } from '@chakra-ui/react';
 
-// const Signup = () => {
-//   const { handleSubmit, register, formState: { errors, isSubmitting } } = useForm();
-
-//   const onSubmit = (values) => {
-//     console.log(values);
-//   };
-
-//   return (
-//     <ChakraProvider>
-//       <Box maxW="md" mx="auto" mt={10} p={8} borderWidth={1} borderRadius={8} boxShadow="lg">
-//         <form onSubmit={handleSubmit(onSubmit)}>
-//           <FormControl isInvalid={errors.name}>
-//             <FormLabel htmlFor="name">Name</FormLabel>
-//             <Input
-//               id="name"
-//               placeholder="Name"
-//               {...register("name", {
-//                 required: "Name is required",
-//               })}
-//             />
-//             <FormErrorMessage>
-//               {errors.name && errors.name.message}
-//             </FormErrorMessage>
-//           </FormControl>
-
-//           <FormControl mt={4} isInvalid={errors.email}>
-//             <FormLabel htmlFor="email">Email</FormLabel>
-//             <Input
-//               id="email"
-//               type="email"
-//               placeholder="Email"
-//               {...register("email", {
-//                 required: "Email is required",
-//                 pattern: {
-//                   value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
-//                   message: "Invalid email address",
-//                 },
-//               })}
-//             />
-//             <FormErrorMessage>
-//               {errors.email && errors.email.message}
-//             </FormErrorMessage>
-//           </FormControl>
-
-//           <FormControl mt={4} isInvalid={errors.password}>
-//             <FormLabel htmlFor="password">Password</FormLabel>
-//             <Input
-//               id="password"
-//               type="password"
-//               placeholder="Password"
-//               {...register("password", {
-//                 required: "Password is required",
-//                 minLength: { value: 6, message: "Password must be at least 6 characters" },
-//               })}
-//             />
-//             <FormErrorMessage>
-//               {errors.password && errors.password.message}
-//             </FormErrorMessage>
-//           </FormControl>
-
-//           <Button mt={4} colorScheme="teal" isLoading={isSubmitting} type="submit">
-//             Sign Up
-//           </Button>
-//         </form>
-//       </Box>
-//     </ChakraProvider>
-//   );
-// };
-
-// export default Signup;
 import React from 'react';
 import { useForm } from 'react-hook-form';
 import { ChakraProvider, Box, Button, FormControl, FormLabel, Input, FormErrorMessage } from '@chakra-ui/react';
 
 const Signup = () => {
-  const { handleSubmit, register, formState: { errors, isSubmitting } } = useForm();
+const { handleSubmit, register, formState: { errors, isSubmitting } } = useForm();
+const email = document.getElementById('email').value;
+const submit = document.getElementById('submit');
+submit.addEventListener("click", clickMe)
 
+
+function clickMe(){
+    this.preventDefault()
+    alert("Done!!")
+}
   const onSubmit = (values) => {
     console.log(values);
   };
---
+++ travelbook/src/firebase.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBH2yfZ-HOgEnN0GOLTonk0OTuGy_QbDgE",
+  authDomain: "travelbook-28a45.firebaseapp.com",
+  projectId: "travelbook-28a45",
+  storageBucket: "travelbook-28a45.appspot.com",
+  messagingSenderId: "396099597162",
+  appId: "1:396099597162:web:28a884d0e98b8bb86aeb13",
+  measurementId: "G-6WLPC9LM23"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+
+
