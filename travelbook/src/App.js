da7a849f8d86e3386d44e68cc636283aface37f8
Bhuvana K S <bhuvana.ks@rockwellautomation.com>
2024-07-16 23:56:23 +0530

    firebase-added

--- travelbook/src/App.js
+++ travelbook/src/App.js
@@ -2,15 +2,7 @@ import React, { useEffect, useState } from "react";
 import './App.css';
 import Signup from "./Pages/Signup";
 import { ChakraProvider } from '@chakra-ui/react'
-const email = document.getElementById('email').value;
-const submit = document.getElementById('submit');
-submit.addEventListener("click", clickMe)
 
-
-function clickMe(){
-    this.preventDefault()
-    alert("Done!!")
-}
 function App() {
   return(
   <ChakraProvider>
